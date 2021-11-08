4c8c0f47507d4f6eb28666322ce39d78bf461ba5
Felix Pacheco <devfex.19@gmail.com>
2021-11-08 10:55:21 -0400

    Implement GiftList

---
+++ src/components/GiftList.tsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import useSession from 'src/hooks/useSession';
+import styled from 'styled-components';
+import { addGift, toggleReservation } from 'src/services';
+import { v4 as uuidv4 } from 'uuid';
+import Gift from './Gift';
+import ButtonStyled from './styled/Button';
+
+const ListContainer = styled.div`
+    max-width: 880px;
+    margin: auto;
+`;
+
+const Header = styled.div`
+    padding: 0;
+    > h1 {
+        font-size: 4rem;
+        margin: .5rem 0;
+        text-align: right;
+        color: #00b3b3;
+    }
+`;
+
+const AddNewGift = styled.div`
+    padding: 1rem 0;
+    text-align: right;
+`;
+
+const GiftList = (): JSX.Element => {
+    const { getInitialState } = useSession();
+    const [state, setState] = useState(() => getInitialState());
+    const { users, gifts, currentUser } = state;
+
+    const handleAdd = () => {
+        const description = prompt('Gift to add');
+        if (description) {
+            setState(addGift(state, uuidv4(), description));
+        }
+    };
+
+    const handleReserve = (id: string) => {
+        setState(toggleReservation(state, id));
+    };
+    return (
+        <ListContainer>
+            <Header>
+                <h1>{`Hi, ${currentUser?.name}`}</h1>
+            </Header>
+            <AddNewGift>
+                <ButtonStyled type="button" onClick={handleAdd}>Add new gift</ButtonStyled>
+            </AddNewGift>
+            <div className="gifts">
+                {gifts.map((gift) => (
+                    <Gift
+                        key={gift.id}
+                        gift={gift}
+                        users={users}
+                        currentUser={currentUser}
+                        onReserve={handleReserve}
+                    />
+                ))}
+            </div>
+        </ListContainer>
+    );
+};
+
+export default GiftList;
