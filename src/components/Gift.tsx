ecaf85c1f3055b6607e12ff042d8dc57ed5f3efe
Felix Pacheco <devfex.19@gmail.com>
2021-11-08 10:55:31 -0400

    Implement Gift

---
+++ src/components/Gift.tsx
@@ -0,0 +1,73 @@
+import { FC } from 'react';
+import styled from 'styled-components';
+import { IGift, IUser } from 'types';
+import ButtonStyled from './styled/Button';
+
+const ContainerGift = styled.div<{ isReserved: boolean }>`
+    border-radius: 4px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-bottom: 20px;
+
+    > img {
+        height: 80px;
+        width: 80px;
+        object-fit: cover;
+        border: 4px solid orange;
+        border-radius: 80px;
+    }
+
+    .description {
+        text-align: center;
+        flex-grow: 1;
+        text-decoration: ${({ isReserved }) => (isReserved ? 'line-through' : 'none')};
+        color: ${({ isReserved }) => (isReserved ? 'gray' : '#00b3b3')};
+    }
+
+    .reservation {
+        width: 120px;
+        text-align: center;
+
+        span {
+            font-size: 30pt;
+
+            &::before {
+                content: "reserved by";
+                font-size: 12pt;
+                color: gray;
+            }
+        }
+    }
+`;
+
+interface IGiftProps {
+    gift: IGift;
+    users: Array<IUser>;
+    currentUser: IUser | null;
+    onReserve: (id: string) => void;
+}
+
+const Gift: FC<IGiftProps> = ({
+    gift, users, currentUser, onReserve,
+}) => (
+    <ContainerGift isReserved={!!(gift.reservedBy)}>
+        <img src={gift.image} alt="gift" />
+        <h2 className="description">{gift.description}</h2>
+        <div className="reservation">
+            {!gift.reservedBy || gift.reservedBy === currentUser?.id ? (
+                <ButtonStyled
+                    type="button"
+                    color="#2b6aa5"
+                    onClick={() => onReserve(gift.id)}
+                >
+                    {gift.reservedBy ? 'Unreserve' : 'Reserve'}
+                </ButtonStyled>
+            ) : (
+                <span>{users[gift.reservedBy].name}</span>
+            )}
+        </div>
+    </ContainerGift>
+);
+
+export default Gift;
