b609835744fe3ea029f86c866dcfffcc8b8c54f8
Felix Pacheco <devfex.19@gmail.com>
2021-11-08 12:29:43 -0400

    Refactor and import properties from useGift hook

--- src/components/GiftList.tsx
+++ src/components/GiftList.tsx
@@ -1,65 +1,37 @@
-import { useCallback, useState } from 'react';
-import useSession from 'src/hooks/useSession';
 import styled from 'styled-components';
-import { addGift, toggleReservation } from 'src/services';
-import { v4 as uuidv4 } from 'uuid';
+import useGift from 'src/hooks/useGift';
 import Gift from './Gift';
 import ButtonStyled from './styled/Button';
 
 const ListContainer = styled.div`
     max-width: 880px;
     margin: auto;
-`;
-
-const Header = styled.div`
-    padding: 0;
     > h1 {
         font-size: 4rem;
         margin: .5rem 0;
         text-align: right;
         color: #00b3b3;
     }
-`;
 
-const AddNewGift = styled.div`
-    padding: 1rem 0;
-    text-align: right;
+    .add-new-gift {
+        padding: 1rem 0;
+        text-align: right;
+    }
 `;
 
 const GiftList = (): JSX.Element => {
-    const { getInitialState } = useSession();
-    const [state, setState] = useState(() => getInitialState());
-    const { users, gifts, currentUser } = state;
-
-    const handleAdd = useCallback(() => {
-        const description = prompt('Gift to add');
-        if (description) {
-            setState((prevState) => addGift(prevState, uuidv4(), description));
-        }
-    }, []);
-
-    const handleReserve = useCallback((id: string) => {
-        setState((prevState) => toggleReservation(prevState, id));
-    }, []);
+    const {
+        users, gifts, currentUser, handleAdd,
+    } = useGift();
 
     return (
         <ListContainer>
-            <Header>
-                <h1>{`Hi, ${currentUser?.name}`}</h1>
-            </Header>
-            <AddNewGift>
+            <h1>{`Hi, ${currentUser?.name}`}</h1>
+            <div className="add-new-gift">
                 <ButtonStyled type="button" onClick={handleAdd}>Add new gift</ButtonStyled>
-            </AddNewGift>
-            <div className="gifts">
-                {gifts.map((gift) => (
-                    <Gift
-                        key={gift.id}
-                        gift={gift}
-                        users={users}
-                        currentUser={currentUser}
-                        onReserve={handleReserve}
-                    />
-                ))}
+            </div>
+            <div>
+                {gifts.map((gift) => <Gift key={gift.id} gift={gift} users={users} />)}
             </div>
         </ListContainer>
     );
