a592dab69d09be534f28594637df0ad303712d8c
Felix Pacheco <devfex.19@gmail.com>
2021-11-08 10:55:10 -0400

    Import giftList

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import GiftList from 'src/components/GiftList';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <GiftList />
     </React.StrictMode>,
     document.getElementById('root'),
 );
